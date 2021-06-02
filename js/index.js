0f8ce365d7b955f0afb7503f095400ac7889be3c
aleupons <apahoquei@gmail.com>
2021-06-02 11:39:10 +0200

    Temps que falta pel bus

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const appId = "0031bdb3";
+const appKey = "1eef12f633798f1b3f0427e9c6e39525";
+const api = "https://api.tmb.cat/v1/ibus/stops/2775?";
+fetch(`${api}app_id=${appId}&app_key=${appKey}`)
+  .then((response) => response.json())
+  .then((dades) => {
+    const tempsAutobus = dades.data.ibus[0];
+    const linia = tempsAutobus.line;
+    const temps = tempsAutobus["text-ca"];
+    return tempsQueFalta(linia, temps);
+  });
+
+const tempsQueFalta = (linia, temps) =>
+  console.log(`Falten ${temps} perquè arribi el bus de la línia ${linia}`);
